0eec359cad852f21f8600f2c4eb2d015c08e4e5e
Christian Wansart <christian.wansart@openknowledge.de>
2020-03-15 23:38:50 +0100

    Remove target folder

--- jakartaee/target/classes/archetype-resources/src/main/java/application/JaxRsActivator.java
+++
@@ -1,8 +0,0 @@
-package ${package}.application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JaxRsActivator extends Application {
-}
